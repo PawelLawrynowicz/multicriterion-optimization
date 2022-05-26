a5120b4ff19a248427dc9c06b8a9e9cccbb3e374
Paweł Ławrynowicz <pawellawrynowicz@protonmail.com>
2022-05-26 14:25:52 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# 1356 2456
+"""
+Zadanie 1: KO: 1, 3
+Zadanie 2: KO: 1, 3, 5
+Zadanie 3: Wszystko
+
+Kryteria optymalizacji:
+1. Czas zakończenia wszystkich zadań (makespan).
+3. Maksymalne spóźnienie zadania (max tardiness).
+5. Maksymalna nieterminowość zadania (max lateness).
+6. Suma nieterminowości zadań (total lateness).
+
+Metody wizualizacji:
+2. Ścieżki wartości (value paths)
+4. Współrzędne gwiazdowe (star coordinates)
+5. Odcinkowe współrzędne gwiazdowe (star coordinate system with line segments)
+6. Wykresy pajęczynowe (spider web charts).
+"""
---
+++ src/problem.py
@@ -0,0 +1,24 @@
+
+from RandomNumberGenerator import RandomNumberGenerator
+import math
+import numpy as np
+
+
+class Problem:
+    def __init__(self, n, seed):
+        rng = RandomNumberGenerator(seed)
+        A = 0
+        self.p = np.zeros((3, n))
+        self.d = np.zeros(n)
+        for i in range(3):
+            for j in range(n):
+                self.p[i][j] = rng.nextInt(1, 99)
+                A = A+self.p[i][j]
+        B = math.floor(A/2)
+        A = math.floor(A/6)
+        for j in range(n):
+            self.d[j] = rng.nextInt(A, B)
+
+
+problem = Problem(10, 1410)
+print(problem.d.shape, problem.p.shape)
